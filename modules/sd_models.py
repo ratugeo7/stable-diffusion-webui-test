cb8f69a8bd9e46b20af01d014be2ce842192a1a0
jayshabibi <jaishabibi@webmail.umm.ac.id>
2023-03-22 14:24:40 +0700

    .

--- modules/sd_models.py
+++ modules/sd_models.py
@@ -238,7 +238,7 @@ def read_state_dict(checkpoint_file, print_global_state=False, map_location=None
     _, extension = os.path.splitext(checkpoint_file)
     if extension.lower() == ".safetensors":
         device = map_location or shared.weight_load_location or devices.get_optimal_device_name()
-        if device == "xla:1":
+        if "xla" in device:
             device = "cpu"
         pl_sd = safetensors.torch.load_file(checkpoint_file, device=device)
     else:
